5101819067d8755a4859a3b55fa773b578f72531
LUCAS RICARDO DOMINGOS ALVES <lucas.ricardo.domingos.alves@univille.br>
2023-08-28 19:28:31 -0300

    first commit

---
+++ src/main/java/br/edu/univille/orcamento_venda/OrcamentoVendaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univille.orcamento_venda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentoVendaApplication.class, args);
+	}
+
+}
