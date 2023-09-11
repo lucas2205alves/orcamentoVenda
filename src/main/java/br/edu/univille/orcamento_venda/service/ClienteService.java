bb208a90a6c8b50fb0e78dde480337559bb82a61
LUCAS RICARDO DOMINGOS ALVES <lucas.ricardo.domingos.alves@univille.br>
2023-09-11 20:34:11 -0300

    WIP: service cliente

---
+++ src/main/java/br/edu/univille/orcamento_venda/service/ClienteService.java
@@ -0,0 +1,10 @@
+package br.edu.univille.orcamento_venda.service;
+
+import java.util.List;
+
+import br.edu.univille.orcamento_venda.entity.Cliente;
+
+public interface ClienteService {
+    List<Cliente> getAll();
+
+}
---
+++ src/main/java/br/edu/univille/orcamento_venda/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,16 @@
+package br.edu.univille.orcamento_venda.service.impl;
+
+import java.util.List;
+
+import br.edu.univille.orcamento_venda.entity.Cliente;
+import br.edu.univille.orcamento_venda.service.ClienteService;
+
+public class ClienteServiceImpl implements ClienteService {
+
+    @Override
+    public List<Cliente> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+}
