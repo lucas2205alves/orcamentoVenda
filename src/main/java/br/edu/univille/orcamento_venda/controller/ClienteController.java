e7c6ac5111fa8fc2b00b8b8ea79404b0f2c7f789
LUCAS RICARDO DOMINGOS ALVES <lucas.ricardo.domingos.alves@univille.br>
2023-09-14 20:31:41 -0300

    WIP: Cliente Service

--- src/main/java/br/edu/univille/orcamento_venda/controller/ClienteController.java
+++ src/main/java/br/edu/univille/orcamento_venda/controller/ClienteController.java
@@ -1,17 +1,28 @@
 package br.edu.univille.orcamento_venda.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.edu.univille.orcamento_venda.entity.Cliente;
+import br.edu.univille.orcamento_venda.repository.ClienteRepository;
+import br.edu.univille.orcamento_venda.service.ClienteService;
+
 @Controller
 @RequestMapping("/clientes")
 public class ClienteController {
 
-    @GetMapping
+    @Autowired
+    private ClienteService service;
+
     public ModelAndView index() {
-        return new ModelAndView("cliente/index");
+
+        var listaClientes = service.getAll();
+        return new ModelAndView("cliente/index", "listaClientes", listaClientes);
     }
 
 }
--- src/main/java/br/edu/univille/orcamento_venda/service/impl/ClienteServiceImpl.java
+++ src/main/java/br/edu/univille/orcamento_venda/service/impl/ClienteServiceImpl.java
@@ -2,15 +2,20 @@ package br.edu.univille.orcamento_venda.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import br.edu.univille.orcamento_venda.entity.Cliente;
+import br.edu.univille.orcamento_venda.repository.ClienteRepository;
 import br.edu.univille.orcamento_venda.service.ClienteService;
 
 public class ClienteServiceImpl implements ClienteService {
 
+    @Autowired
+    private ClienteRepository repository;
+
     @Override
     public List<Cliente> getAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        return repository.findAll();
     }
 
 }
