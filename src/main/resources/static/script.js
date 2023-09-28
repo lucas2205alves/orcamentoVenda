e0bfa8e2d0240a4f55c40cbe39e0c884ccf371c2
LUCAS RICARDO DOMINGOS ALVES <lucas.ricardo.domingos.alves@univille.br>
2023-09-28 20:34:18 -0300

    WIP: Javascript delete

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,3 +1,7 @@
 (function(){
-
-})()
+    $("#tabclientes").on("click",".js-delete", function{
+        let botaoClicado = $(this);
+        $("#btnsim").attr("data-id", botaoClicado.attr("data-id"));
+        $("#modalcliente").modal("show");
+    });
+})();
